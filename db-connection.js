12a834da15d97d0ce0be7c1dcc91e057c2a50567
FPierre <pflauder@gmail.com>
2017-07-04 22:41:42 +0200

    Mise en place de la DB sqlite dans les requetes

---
+++ db-connection.js
@@ -0,0 +1,28 @@
+const Sequelize = require('sequelize')
+
+// const db = []
+const sequelize = new Sequelize(
+  'database',
+  'username',
+  'password', {
+    host: 'localhost',
+    dialect: 'sqlite',
+    pool: {
+      max: 5,
+      min: 0,
+      idle: 10000
+    },
+    storage: './users.sqlite'
+  }
+)
+/*
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.')
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err)
+  })
+*/
+module.exports = sequelize
--- user-service.js
+++ user-service.js
@@ -1,29 +1,15 @@
 const cote = require('cote')
 
+const User = require('./user')
+
 const responder = new cote.Responder({ name: 'user responder', key: 'user' })
 
-// DB mocking
-const users = [
-  { id: 1, name: 'pierre', password: 'pierre' }
-]
-
 responder.on('index', ({ type }, cb) => {
-  return new Promise((resolve, reject) => {
-    resolve(users)
-    reject('rejected')
-  })
+  return User.findAll()
 })
 
 responder.on('show', ({ type, id }, cb) => {
-  const user = users.find(u => u.id === id)
-
-  return new Promise((resolve, reject) => {
-    if (user) {
-      resolve(user)
-    } else {
-      reject('rejected')
-    }
-  })
+  return User.findById(id)
 })
 
 responder.on('login', ({ type, user }, cb) => {
